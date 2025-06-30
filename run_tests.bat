de282e94ed866e78eda28d77e902deae8c5a7df5
James Johnson <vstep@pacbell.net>
2025-06-30 09:01:30 -0700

    Added use case with 'help' tag.

--- run_tests.bat
+++ run_tests.bat
@@ -1,3 +1,4 @@
+python .\lmo.py -h
 python .\lmo.py -s 12345 -v 1 -t 1 -i .\data\input\in.txt -o .\data\output\out1.txt
 python .\lmo.py -s 12345 -v 1 -t 2 -i .\data\input\in.txt -o .\data\output\out2.txt
 python .\lmo.py -s 12345 -v 1 -t 3 -i .\data\input\in.txt -o .\data\output\out3.txt
